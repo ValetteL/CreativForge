a24bb1108e170c21ea701fdf8e68a4d59038918b
ValetteL <louis.valette2017@gmail.com>
2025-08-07 18:22:04 +0200

    restruct project

--- DTOs/PromptDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CreativForge.DTOs
-{
-    public class PromptDto
-    {
-        public string Theme { get; set; }
-        public string Constraint { get; set; }
-        public string Format { get; set; }
-    }
-}
--- DTOs/RegenerateFieldRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace CreativForge.DTOs
-{
-    public class RegenerateFieldRequest
-    {
-        public string Field { get; set; } // ex: "theme", "constraint", "format", "all"
-        public PromptDto Current { get; set; }
-    }
-}
